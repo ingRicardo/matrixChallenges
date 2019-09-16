19b4f5f8dadf424664d7a9d119d29f022e6ec190
Ricardo Alfredo Macias Olvera <ra_mo116@hotmail.com>
2019-09-16 15:05:13 -0700

    rolling the ball
    optimized version

--- rollingBall3.cpp
+++ rollingBall3.cpp
@@ -1,31 +1,26 @@
-// rollingBall3.cpp : Defines the entry point for the console application.
-//
-
-#include "stdafx.h"
 #include <iostream>
-#include  <stdio.h>
 #include <fstream>
 using namespace std;
-void print_matrix(int N, int **m){
 
-	for (int row = 0; row< N; row++){
+void print(int ** grid, int N){
 
-		cout << "\n";
-
-		for (int col = 0; col <N; col++){
-			cout << " " << m[row][col];
-
+	for(int i =0; i < N; i++){
+		cout<< "\n";
+		for(int j=0; j<N; j++){
+			cout<< " "<< grid[i][j];
 		}
 	}
-	cout << endl;
+	
+	cout<<endl;
 
+}
+bool isValid(int N, int row, int col ){
+
+	return row>-1 && row<N && col>-1 && col<N ;
 
 }
 
-bool isValid(int N, int row, int  col){
 
-	return row > -1 && row < N && col> -1 && col < N;
-}
 int findPath(int **grid, int row, int col, int N){
 
 	int answer = 1;
@@ -60,79 +55,58 @@ int findPath(int **grid, int row, int col, int N){
 		row += deltaRow[newDirection];
 		col += deltaCol[newDirection];
 
-		if (!isValid(N, row, col)){
+		if (!isValid(N, row, col) || smallest < grid[row][col])
 			break;
-		} else {
-			if (smallest < grid[row][col])
-				break;
-			else
-				answer++;
-		}
+		 else 
+			answer++;
 
 	}
 
 	return answer;
 }
 
-int findMax(int **m, int N){
-
-	int maxElem = m[0][0];
-
-	for (int i = 0; i < N; i++)
-		for (int j = 0; j < N; j++)
-			if (m[i][j] > maxElem)
-				maxElem = m[i][j];
-
-
-	return maxElem;
-}
 int main(){
 
 	freopen("input.txt", "r", stdin);
 
-	int test_case = 0, N = 0, i, j, num;
+	int test_case =0,N=0,i,j,num;
 	cin >> test_case;
-	for (int tc = 1; tc <= test_case; tc++){
-		cout << "\ntest case " << tc << "\n";
+
+	for(int tc=1; tc <= test_case; tc++){
+		cout<< "test case "<< tc;
+		cout<<endl;
+
 		cin >> N;
-		int **matrix = new int*[N];
+		int **grid = new int*[N];
 
-		for (int i = 0; i< N; i++)
-			matrix[i] = new int[N];
+		for(int i=0; i<N; i++)
+			grid[i] = new int[N];
 
-		for (i = 0; i < N; i++){
-			for (j = 0; j < N; j++){
+
+		for(int i=0; i<N; i++)
+			for(int j=0; j<N; j++){
 				cin >> num;
-				matrix[i][j] = num;
+				grid[i][j] = num;
+			}
+		
+		print(grid, N);
+		int row=0 , col=0, max; 
+		max = findPath(grid,row,col,N);
+
+		for( row =0 ; row< N; row++){
+				cout<<"\n";
+			for( col=0; col<N; col++){
+				if(  findPath(grid,row,col,N) > max ){
+					max = findPath(grid,row,col,N);	
+				}
+				cout << " "<< findPath(grid,row,col,N);
+				
 			}
 
 		}
-		print_matrix(N, matrix);
-		int idxcc = 0, max = 0;
-
-		int **pathMatr = new int*[N];
-		for (int i = 0; i < N; i++)
-			pathMatr[i] = new int[N];
-
-
-		for (int row = 0; row < N; row++){
-			for (int col = 0; col< N; col++){
-				idxcc = findPath(matrix, row, col, N);
-				pathMatr[row][col] = idxcc;
-			}
-
-		}
-
-	//	cout << "\n testing findpath \n" << findPath(matrix, 0, 0, N);
-	//	cout << "\n";
-
-		cout << "\n print idx matrix \n ";
-
-		print_matrix(N, pathMatr);
-
-		max = findMax(pathMatr, N);
-		cout << "#"<<tc <<":"<< max << "\n";
-		cout << endl;		
+		cout <<endl;
+		cout << "#"<<tc << ":"<< max;
+		cout << endl;
 
 	}
 
