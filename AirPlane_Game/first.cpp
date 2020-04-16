38ce7acd7280be3bb5c6ff122f206ab97f9febe7
Ricardo <ra_mo116@hotmail.com>
2020-04-15 20:07:41 -0700

    First Analisis

---
+++ AirPlane_Game/first.cpp
@@ -0,0 +1,31 @@
+/*
+		Airplane Game
+		Ricardo Alfredo Macias Olvera
+		Software Engineer
+*/
+#include <iostream>
+using namespace std;
+
+int main()
+{
+
+
+	/*
+	 *	height =N , width = 5
+	 *	DELIMITED AREA = 5X5
+	 * 	GAME AREA = x =  startHeighValue = N-width , y = width 
+	 *	
+	 *	CONTROL AREA = Initial position (coordenates x,y)
+	 *	width of the map is always five and the airplane starts at center			
+	 *			center	x=0,y=2
+	 *	MOVES = for loop  left,right,0
+	 *	MAP DESCENDS ONE STEP = if(x!=0) x-=1 
+	 *
+	 *
+	 *
+	 *
+	 */
+
+	cout << "Airplane Game under construction "<<endl;
+	return 0;
+}
