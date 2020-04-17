982c555513567402b3c8e5c2dfd23ac1ceb1c8e6
Ricardo <ra_mo116@hotmail.com>
2020-04-16 20:00:25 -0700

    more ideas

--- AirPlane_Game/first.cpp
+++ AirPlane_Game/first.cpp
@@ -14,16 +14,36 @@ int main()
 	 *	height =N , width = 5
 	 *	DELIMITED AREA = 5X5
 	 * 	GAME AREA = x =  startHeighValue = N-width , y = width 
-	 *	
-	 *	CONTROL AREA = Initial position (coordenates x,y)
+	 *		variable height_START every step DECREASE
+	 *	2. 4.	variable height_END   every step DECREASE
+	 *	CONTROL AREA = Initial position (coordenates x,y)   
 	 *	width of the map is always five and the airplane starts at center			
 	 *			center	x=0,y=2
-	 *	MOVES = for loop  left,right,0
-	 *	MAP DESCENDS ONE STEP = if(x!=0) x-=1 
+	 *      1. MOVES = for loop  left,right,0
+	 *	INITIAL POSITION MAP DESCENDS ONE STEP = if(x!=0) x-=1  
+	 *	3. IF ( coins < 0 )
+	 *		RETURN
+	 *
+	 *
+	 *	IF( position == 2 ) 
+	 *		coins -=1 
+	 *	ELSE IF (position == 1 )
+	 *		coins+=1;
+	 *
+	 *	0
+	 *      0
+	 *      X,Y   ^
 	 *
 	 *
 	 *
+	 *	0    ^
+	 *      X,Y  ^
+	 *     
+	 *    5. IF height_START == height_END 
+	 *    		RETURN
 	 *
+	 *    	IF LEFT ==2 AND RIGHT ==2 AND CURRENT =2
+	 *    		USE THE BOMB 	
 	 */
 
 	cout << "Airplane Game under construction "<<endl;
