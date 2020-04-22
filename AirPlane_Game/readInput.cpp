976dfab45e9d9b6059694670d1b301516240c85f
Ricardo <ra_mo116@hotmail.com>
2020-04-22 08:59:24 -0700

    augmented grid

--- AirPlane_Game/readInput.cpp
+++ AirPlane_Game/readInput.cpp
@@ -20,53 +20,71 @@ bool isValid(int **grid,int row, int col, int h, int w)
 
 }
 
-void funcRecTest1(int **grid,int &coins,int row, int col, int &h, int w, int &hX)
+void funcRecTest1(int **&gridAux,int &coins,int row, int col, int &h, int w, int &hX)
 {
 
-	int current = grid[row][col];
+	int current = gridAux[row][col];
         int deltaX[] = {0 ,0,0};
         int deltaY[] = {-1,1,0};
         int dsize =  sizeof(deltaX)/sizeof(deltaX[0]);
 	
-	cout << "current -> "<< current << "[ row "<<row << ", col "<<col<<"]"<<endl;
 
 
 	/*
 	 *	1. move to the left, right or stay
-	 *	2. check how many enemies are here and if there are coins
+	 *	2. check how many enemies are here 
+	 *		if(enemies == 2 )
+	 *			apply the bomb
+	 *
 	 *	3. moves one step up.
+	 *
+	 *
+	 *	IM AM ON THE CENTER IN THE CONTROL AREA (what is the control area?)
+	 *	MOVE RIGHT 
+	 *	MOVE UP ONE STEP ON THE INFERIOR LIMIT AND MOVE UP ONE STEP ON THE SUPERIOR LIMIT OF THE GAME AREA
+	 *
+	 *	
+	 *	IF CURRENT == COIN 
+	 *		ADD 1 TO COINS
+	 *
+	 *
+	 *
+	 *
+	 *
 	 */
-	int enemies =0;
-	for(int idx =0; idx < dsize; idx++)
+
+	if(coins <0 )
 	{
-		int nextRow = row + deltaX[idx];
-		int nextCol = col + deltaY[idx];
-		if(isValid(grid,nextRow,nextCol,h,w))
+
+		return;
+	}
+
+	if(current == 1)
+		coins+=1;
+	else if (current == 2)
+		coins-=1;
+	
+
+	int enemies =0,nextRow=0,nextCol=0;
+	bool validUP = false, bomb = false;
+	for(int idx =0; idx < dsize; idx++)//move horizontal 
+	{
+		 nextRow = row + deltaX[idx] -1 ;
+		 nextCol = col + deltaY[idx];
+		if(isValid(gridAux,nextRow,nextCol,h,w))
 		{
-			cout << "valid "<<endl;
-			int next = grid[nextRow][nextCol];
-			if(next == 2)
-			{
-				enemies+=1;
-				coins-=1;
-			}
-			else if (next == 1)
-			{
+			int next = gridAux[nextRow][nextCol];
+			cout << "valid "<<next<<":"<<nextRow<<","<<nextCol<<endl;
+			if(hX>0)
+				hX-=1;//decrease superior limit 	
 
-				coins+=1;
-				//break;
-				// moves UP
-				//recursion OR break ??
+			h-=1;//decrease inferior limit
+			
+			funcRecTest1(gridAux,coins,nextRow, nextCol,h,w,hX);
 
-				if (isValid(grid,nextRow-1,nextCol,h,w))//moves UP in ROW 
-				{
-					cout << "VALID UP "<<endl;
-			 		//funcRecTest1(grid,coins,row, col,h,w, hX);
-				}
-			}
 		}
 	}
-	cout << " coins "<<coins << " enemies "<<enemies <<endl;
+
 }
 
 void func1(int **grid, int h, int &res)
@@ -77,14 +95,43 @@ void func1(int **grid, int h, int &res)
 int hX = h-5;// above limit
 int w = 5;
 
-int curX = h -1; //start position in X
-int curY = 2;	//start position in Y
 int coins =0;
+int hAux =h+1;
 cout << "top limit  "<< hX << " width "<<w << " height "<<h <<endl;
-printGrid(grid,h,w,0);
+int **gridAux = new int*[hAux];
+	for(int row =0; row<hAux; row++)
+	{
+		gridAux[row] = new int[hAux];
+		for(int col=0; col<5; col++)
+		{
+			gridAux[row][col] = 0;
+		}
 
+	}
+	
+	for(int row =0; row<h; row++)
+		{
+			for(int col=0; col<5; col++)
+			{
+				gridAux[row][col] = grid[row][col];
+			}
 
-funcRecTest1(grid,coins,curX,curY,h,w,hX);
+		}
+cout<<endl;
+cout <<"Augmented grid "<<endl;
+printGrid(gridAux,hAux,w,0);
+
+cout<<endl;
+cout << "game Area "<<endl;
+printGrid(gridAux,hAux,w,hX);
+
+int curX = hAux -1; //start position in X
+int curY = 2;	//start position in Y
+cout << "current position -> "<< gridAux[curX][curY] <<" : "<< curX<<","<<curY<<endl;
+
+funcRecTest1(gridAux,coins,curX,curY,h,w,hX);
+cout << endl;
+printGrid(grid,h,w,hX);
 
 }
 int main()
