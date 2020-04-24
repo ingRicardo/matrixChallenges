b98600931412d36c13bd28cae445242d3c41a1da
Ricardo <ra_mo116@hotmail.com>
2020-04-24 09:01:37 -0700

    getting more ideas of applying the bomb

--- AirPlane_Game/readInput.cpp
+++ AirPlane_Game/readInput.cpp
@@ -20,12 +20,13 @@ bool isValid(int **grid,int row, int col, int h, int w)
 
 }
 
-void funcRecTest1(int **&gridAux,int &coins,int row, int col, int &h, int w, int &hX, bool &bomb, bool &isPath)
+void funcRecTest1(int **&gridAux,int &coins,int row, int col, int &h, int w, int &hX, bool &bomb, bool &isPath, int &prevDirec)
 {
 
 	int current = gridAux[row][col];
         int deltaX[] = {0 ,0,0};
-        int deltaY[] = {1,-1,0};
+        int deltaY[] = {1,0,-1};
+
 
         int dsize =  sizeof(deltaX)/sizeof(deltaX[0]);
 	
@@ -50,6 +51,23 @@ cout << "current  -> "<< gridAux[row][col] <<" : "<< row<<","<<col<<endl;
 	 *		ADD 1 TO COINS
 	 *
 	 *
+	 *		1-move (right/stay/left)
+	 *		2-check if there is a coin 
+	 *			2.1 -move up
+	 *			2.2 -add one to coin
+	 *			
+	 *		3-check if there is an enemy and bomb == false
+	 *			3.1 -apply the bomb and bomb == true
+	 *			     move up
+	 *			else if there's an enemy and bomb == true
+	 *			     move up
+	 *			     substract one to coin
+	 *
+	 *		4-go back to first step
+	 *
+	 *		-----------------------------------------
+	 *		HOW IT'S MOVING HORIZONTALLY?
+	 *		IF IT MOVED TO SPECIFIC DIRECTION AND THEN RECURSES IT STAYED WITH THE PREVIOUS DIRECTION OR STARTS AGAIN WITH MOVING TO RIGHT
 	 *
 	 *
 	 *
@@ -84,11 +102,25 @@ cout << "current  -> "<< gridAux[row][col] <<" : "<< row<<","<<col<<endl;
 			coins-=1;
 		}
 
+
+		/*
+		 *
+		 *	if bomb == true
+		 *
+		 *		apply bomb
+		 *
+		 *	if current == 2
+		 *		coins =-1
+		 *
+		 *
+		 */
+
+
 	}
 
 	if(coins <0 || h == 0 || h==hX )
 	{
-		cout << " end coins -> "<<coins << " height "<< h<< " hX "<< hX <<" bomb "<<bomb<<" isPath "<<isPath<<endl;
+		cout << " end coins --------------------------------------> "<<coins << " height "<< h<< " hX "<< hX <<" bomb "<<bomb<<" isPath "<<isPath<<endl;
 		return;
 	}
 	isPath = false;
@@ -97,23 +129,50 @@ cout << "current  -> "<< gridAux[row][col] <<" : "<< row<<","<<col<<endl;
 
 // NEEDS TO VALIDATE IF THERE IS AN ENEMY TO APPLY THE BOM BEFORE STARTS TO MOVE
 //
+//
+//	CHANGE THE VALID TO MOVE TO 0,1 AND 2
+//	AND LOOK FOR THE ONES TO MOVE or GET THE PREVIOUS POSITION WHEN THE TURN STARTS AGAIN
+//
+//
+	
+
+	/*
+	 *CHECK IF IN STAY POSITION DOES EXIST AN ENEMY IF TRUE APPLY THE BOMB
+	 *
+	 *
+	 *
+	 **************   APPLY THE BOMB AND THEN MOVE IT ????????? THIS NEEDS FOCUS ********
+	 */ 	
+
 	int noValid =0;
 	int nextRow =0,nextCol =0;
+
+
+	nextRow = (row + deltaX[0]) -1 ;
+	nextCol = col + deltaY[0];
+	if(isValid(gridAux,nextRow,nextCol,h,w) &&  gridAux[nextRow][nextCol] == 2) 
+	{
+		cout << "SHOULD APPLY THE BOMB AT THE START OF THE GAME "<<endl;
+
+	}
+
 	for(int idx =0; idx < dsize; idx++)//move horizontal 
 	{
-		nextRow = row + deltaX[idx] -1 ;
+		nextRow = (row + deltaX[idx]) -1 ;
 		nextCol = col + deltaY[idx];
 		noValid+=1;
 		if(isValid(gridAux,nextRow,nextCol,h,w) && ( gridAux[nextRow][nextCol] == 1 ||  gridAux[nextRow][nextCol]==2    )   )
 		{
 			isPath = true;
 			int next = gridAux[nextRow][nextCol];
-			cout << "valid "<<next<<":"<<nextRow<<","<<nextCol<<endl;
+			prevDirec = idx;
+			cout << "valid -----------> "<<next<<": coordinates "<<nextRow<<","<<nextCol<<" moved to ->"<<idx<<endl;
 			if(hX>0)
 				hX-=1;//decrease superior limit 	
 
+
 			h-=1;//decrease inferior limit
-			funcRecTest1(gridAux,coins,nextRow, nextCol,h,w,hX,bomb,isPath);
+			funcRecTest1(gridAux,coins,nextRow, nextCol,h,w,hX,bomb,isPath,prevDirec);
 
 		}
 	}
@@ -122,12 +181,20 @@ cout << "current  -> "<< gridAux[row][col] <<" : "<< row<<","<<col<<endl;
 	if(isValid(gridAux,nextRow,nextCol,h,w) && gridAux[nextRow][nextCol] == 0)
 	{
 
-		cout << " -> "<<gridAux[nextRow][nextCol]<<endl; 
-		cout << "end THERE IS NO PATH coins "<<coins << " bomb "<<bomb<<  endl;
+	//	cout << " -> "<<gridAux[nextRow][nextCol]<<endl; 
+	//	cout << "end THERE IS NO PATH coins "<<coins << " bomb "<<bomb<<  endl;
 
-		if (bomb && coins == 0)
-			coins =-1;
-		return;
+	//	if (bomb && coins == 0)
+	//		coins =-1;
+	//	return;
+	//
+		if (   gridAux[nextRow][nextCol] ==0) 
+		{
+
+			//coins=-1;
+			//cout << " END -> COINS "<<coins << " bomb "<< bomb <<endl;
+			//return;
+		}
 	}	
 }
 
@@ -174,7 +241,8 @@ int curY = 2;	//start position in Y
 //cout << "current position -> "<< gridAux[curX][curY] <<" : "<< curX<<","<<curY<<endl;
 bool bomb = false;
 bool isPath = true;
-funcRecTest1(gridAux,coins,curX,curY,h,w,hX,bomb,isPath);
+int prevDirec =0;
+funcRecTest1(gridAux,coins,curX,curY,h,w,hX,bomb,isPath,prevDirec);
 cout << endl;
 printGrid(gridAux,h,w,hX);
 cout << " coins -> "<<coins <<" h "<<h << " hX "<<hX<< endl;
