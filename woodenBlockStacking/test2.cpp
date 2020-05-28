65d366dea50e045d60872c35d945242a010cf973
r-macias <r.macias@samsung.com>
2020-05-28 08:47:23 -0700

    updated test2 to run the first test case

--- woodenBlockStacking/test2.cpp
+++ woodenBlockStacking/test2.cpp
@@ -1,39 +1,38 @@
 #include <iostream>
 using namespace std;
-int res=0;
+int res=1;
 
 
-void rec(int n, int ** blocks_arr, int size, bool **visited)
+void rec(int n, int ** blocks_arr, int size, int nextRow, int nextCol,int &cc)
 {
-    if(n >4)
+    if(n >=4)
     {
         cout << "return n "<<n<<endl;
         return;
     }
-    /*
-    int  row = blocks_arr[n][0];
-    int  col = blocks_arr[n][1];
-    cout << " ------ "<<endl;
-    cout << "row " << row<< " col "<< col<<endl;
-    int cc =0;
-   for(int t= 0; t< size; t++)
-   {
-        int  nextrow = blocks_arr[t][0];
-        int  nextcol = blocks_arr[t][1];
-        cout << " nextrow  "<< nextrow << " nextcol "<< nextcol <<endl;
-        if ( (row > nextrow && col >nextcol ) || (row > nextcol && col >nextrow) ||
-              (row >= nextrow && col >nextcol )  || (row > nextcol && col >= nextrow)  )
-        {
-            cc+=1;
-            cout << "contains -> "<< nextrow << ", "<< nextcol <<endl;
-        }
-   }
-    cout << row << ", "<<col << " contains "<< cc<<endl;*/
-    rec(n+1, blocks_arr, size, visited);
+    int r = nextRow;
+    int c = nextCol;
+    cout << " current -> "<< r << ", "<<c <<endl;
+    if (r> blocks_arr[n+1][0] && c > blocks_arr[n+1][1] )
+    {
+        cout << "normal "<<blocks_arr[n+1][0] << ", "<< blocks_arr[n+1][1]<<endl;
+        nextRow = blocks_arr[n+1][0]; nextCol = blocks_arr[n+1][1];
+        cc+=1;
+    }
+    else if (r > blocks_arr[n+1][1] && c > blocks_arr[n+1][0] )
+    {
+        cout << "rotation "<<blocks_arr[n+1][1]<<", "<<  blocks_arr[n+1][0]<<endl;
+        nextRow = blocks_arr[n+1][1];nextCol = blocks_arr[n+1][0];
+        cc+=1;
+
+    }
+    
+    rec(n+1, blocks_arr, size, nextRow, nextCol,cc);
 }
 
 void func2(int ** blocks_arr, int size)
 {
+    cout << "function 2 "<<endl;
 
     bool **visited = new bool*[size];
 
@@ -51,28 +50,36 @@ void func2(int ** blocks_arr, int size)
    // int max [][] ={}; 
   // int **blocks_tmp = new int*[2];
 
-   for (int p=0; p<= size; p++)
-   {
-    int r = blocks_arr[p][0];
-    int c = blocks_arr[p][1];
-    for (int s =p+1; s< size; s++)
-    {
-        if ( (r >  blocks_arr[s][0] && c >blocks_arr[s][1] ) || (r > blocks_arr[s][1] && c >blocks_arr[s][0]) /*||
-                (r >=  blocks_arr[s][0] && c >blocks_arr[s][1]  )  || (r > blocks_arr[s][1] && c >=blocks_arr[s][0])*/  )
-            {
-                cout << "max ->  "<<r <<","<<c <<endl;
-            }
-    }
-   
-   }
-
     int n =0;
-     
-    rec(n, blocks_arr, size, visited);
+    int r = blocks_arr[n][0];
+    int c = blocks_arr[n][1];
 
-   
+    for(int i=0;i<size;i++)
+	{		
+		for(int j=i+1;j<size;j++)
+		{
+			if(blocks_arr[i][0] < blocks_arr[j][0] && 
+               blocks_arr[i][1] < blocks_arr[j][1])
+			{
+				r  =blocks_arr[i][0];
+                c = blocks_arr[i][1];
 
-
+				blocks_arr[i][0]=blocks_arr[j][0];
+                blocks_arr[i][1]=blocks_arr[j][1];
+				blocks_arr[j][0]=r;
+                blocks_arr[j][1]=c;
+			}
+		}
+	}
+	//print sorted array elements
+	cout<<"Sorted (Descending Order) Array elements:"<<endl;
+	for(int i=0;i<size;i++)
+		cout<<blocks_arr[i][0]<<", "<< blocks_arr[i][1] << "\t";
+	cout<<endl;	
+    int cc =1;
+    rec(n, blocks_arr, size, blocks_arr[n][0],blocks_arr[n][1],cc);
+    res = cc;
+    cout << "res ------> "<<res <<endl;
     /*bool **visited = new bool *[row];
 
     for(int r =0; r< row; r++)
@@ -129,7 +136,7 @@ int main()
         func2(blocks_arr,blocks);
 
         cout << "#"<<tc<<":"<<res<<endl;
-        res =0;
+       
 
     }
 
