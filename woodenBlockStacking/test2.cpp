5cacae165cd1caca902834a42be90c36f65a91e5
r-macias <r.macias@samsung.com>
2020-06-10 08:58:57 -0700

    Its getting statistics no logic

--- woodenBlockStacking/test2.cpp
+++ woodenBlockStacking/test2.cpp
@@ -339,10 +339,12 @@ void func2(int ** blocks_arr, int size)
     */
 
   // int ** blocks_arr, int size
-    int restmp =0; 
+    int restmp =0,inside=0,nomatch=0; 
     for (int n = 0; n< size ; n++)
     {
         restmp =0;
+        inside =0;
+        nomatch =0;
         int r = blocks_arr[n][0];
         int c = blocks_arr[n][1];
         cout << "current "<< r << ", "<< c <<endl;
@@ -355,6 +357,7 @@ void func2(int ** blocks_arr, int size)
                     ) 
 
                 {
+                     cout << "  "<<blocks_arr[i][0] << ", "<< blocks_arr[i][1];
                     restmp++;
                     
                 }
@@ -372,18 +375,20 @@ void func2(int ** blocks_arr, int size)
                     
                     ) ) 
                     {
-
+                        cout << "\n --- inside of  "<<blocks_arr[i][0] << ", "<< blocks_arr[i][1]<<endl;
+                        inside++;
                     }
                 else if ( ( (r != blocks_arr[i][0] && c != blocks_arr[i][1]) ||
                     (c != blocks_arr[i][0] && r != blocks_arr[i][1]) ))
                     {
-                        cout << " --- no match "<<blocks_arr[i][0] << ", "<< blocks_arr[i][1]<< endl;
+                        cout << "\n --- no match "<<blocks_arr[i][0] << ", "<< blocks_arr[i][1]<<endl;
+                        nomatch++;
                     }
                     
 
 
         }
-        cout << "contains "<< restmp <<endl;
+        cout << "\n --- contains "<< restmp <<" inside "<< inside << " nomatch "<< nomatch << endl;
     }
     res = restmp;
 }
@@ -398,7 +403,7 @@ int main()
     int t,blocks=0;
     cin>>t;
     cout <<"test  cases "<<t<<endl;
-    for (int tc =1; tc<=4; ++tc)
+    for (int tc =1; tc<=5; ++tc)
     {
         cin>> blocks;
         int **blocks_arr = new int*[blocks];
