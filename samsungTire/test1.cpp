1ecb82f73e9c9a48de698935909a5c50e50cd4f2
r-macias <r.macias@samsung.com>
2020-07-07 08:59:23 -0700

    understanding the idea

--- samsungTire/test1.cpp
+++ samsungTire/test1.cpp
@@ -6,6 +6,33 @@ int main()
 
 	cout << "samsung tire"<<endl;
 
+  /*
+
+	max air pressure k= 100
+	initial pressure = 60
+	
+	inflate = 50  + 60  = 110
+	deflate = 40  
+	
+
+	inflate = 20
+	deflate = 90  
+
+	
+	15 + 80 = 95
+	95 - 95 = 0
+
+	0 + 75 = 75
+	75 - 30 = 45
+
+	45 + 45 = 90
+	90 - 55 = 35
+
+	current_pressure = initial_presure
+	current_pressure = current_pressure + ( inflate - deflate )
+	current_pressure  =  15 + (80 -95) = 15 +(- 15) = 0
+
+  */
 
 return 0;
 }
