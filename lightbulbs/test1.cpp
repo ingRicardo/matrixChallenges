f34107daa99d0dc5d893db190a0826d1aeb4c53d
Ricardo Macias <r.macias@samsung.com>
2020-08-13 09:10:15 -0700

    problem resolved , needs to be clean

--- lightbulbs/test1.cpp
+++ lightbulbs/test1.cpp
@@ -1,7 +1,7 @@
 //https://www.codesdope.com/blog/article/generating-permutations-of-all-elements-of-an-arra/
 #include <iostream>
 using namespace std;
-int res=10000;
+int res=0;
 void printArray(int arr[], int bulbs)
 {
 	for(int i=0; i<bulbs; ++i)
@@ -25,37 +25,46 @@ void permut(int *array,int bulbs, int *switches, int swdown, int start, int end,
 	//if (start == end)
 	if (start == swSize)
 	{
-		printArray(switches,swSize);
+		//printArray(switches,swSize);
 
 		int *arrTmp = new int [end+1];
 		for(int i=0; i<end+1; i++)
 		{
 			arrTmp[i] = array[i]; 
 		}
-		printArray(arrTmp,end+1);
-
+	//	cout << "original :"<<endl;
+	//	printArray(arrTmp,end+1);
 		for (int s=0; s < 3; ++s) // attempts
 		{
-			for (int  i =switches[s] , j =0 ; j < end+1; i+=switches[s], j++)
+			totalBulbs=0;
+			for (int  i =switches[s]-1 , j =0 ; j < end+1; i+=switches[s]+1, j++)
         	{
-				if(arrTmp[i])
+				//cout << " i "<< i <<endl;
+				
+				if( i < end+1 && arrTmp[i] == 1 )
 				{
-					arrTmp[i] = !arrTmp[i];
+					arrTmp[i] = 0;
 				}
-				else
+				else if (i < end+1 && arrTmp[i] == 0 )
 				{
-					arrTmp[i] = arrTmp[i];
+					arrTmp[i] = 1;
 				}
+				
 				if( !arrTmp[j])
 				{
 					totalBulbs+=1;
 				}
+				
 			}
-			printArray(arrTmp,end+1);
-			if ( totalBulbs < res)
+		//	cout << " s "<<s<<endl;
+		//	printArray(arrTmp,end+1);
+		//	cout << " totalBulbs "<< totalBulbs<<endl;
+			if ( totalBulbs > res)
             	res = totalBulbs;
-
+			
 		} 
+		
+		
 		return;
 	}
 	
@@ -76,32 +85,33 @@ void funcTest(int *array,int bulbs, int suitch, int swdown, int start, int end, 
 {
 	int *switches = new int[suitch];
 	int val=1;
-	cout << "initial switches "<<endl;
+	//cout << "initial switches "<<endl;
 	for (int s=0; s< suitch; ++s)
 	{
 		switches[s] = val;
 		val++;
-		cout << " "<< switches[s]; 
+	//	cout << " "<< switches[s]; 
 	}
-	cout <<endl;
-	cout <<endl;
+//	cout <<endl;
+//	cout <<endl;
 
 	permut(array,bulbs, switches, swdown, start, end, totalBulbs, suitch);
 }
 int main()
 {
-	cout <<"test"<<endl;
+	//cout <<"test"<<endl;
 
 	freopen("input.txt","r",stdin);
 	int t;
 	cin>>t;
-	cout <<" t "<< t<<endl;
-	for (int tc=1; tc<=1; ++tc)
+	//cout <<" t "<< t<<endl;
+	for (int tc=1; tc<=t; ++tc)
 	{
 		int bulbs,sw,swdown;
-		int totalBulbs=0;
+		int totalBulbs=10000;
+		res=0;
 		cin >> bulbs;  cin >>sw;
-		cout << " bulbs "<< bulbs<< " switch "<< sw<<endl;
+	//	cout << " bulbs "<< bulbs<< " switch "<< sw<<endl;
 		int *bulbVec = new int [bulbs];
 		for (int b=0; b<bulbs; ++b)
 		{
@@ -111,7 +121,7 @@ int main()
 			
 		}
 		funcTest(bulbVec,bulbs,sw,swdown,0,bulbs-1,totalBulbs);
-		cout << endl;
+	//	cout << endl;
 		cout<<"#"<<tc<<":"<<res<<endl; 
 	}
 	return 0; 
