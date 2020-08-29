f933330a0140c7ebb9116bb8c499486e4ddcd116
Ricardo <ra_mo116@hotmail.com>
2020-08-28 19:52:15 -0700

    calculate comb and perm

---
+++ numbCombination/permuCombi.cpp
@@ -0,0 +1,38 @@
+ #include<iostream>
+
+using namespace std;
+
+int fact(int num) 
+{
+         if(num==1) 
+        {
+              return 1;
+        }
+        else 
+        {
+             return num * fact(num-1);
+        }
+}
+
+int main() 
+{
+       int n,r,nPr,nCr;
+
+       cout<<"Enter n: ";
+       cin>>n;
+       
+       cout<<"Enter r: ";
+       cin>>r;
+
+       nPr=fact(n)/fact(n-r);
+
+       nCr=nPr/fact(r);
+
+       cout<<"nPr is: "<<nPr<<endl;
+       cout<<"nCr is: "<<nCr;
+
+       // r = digits
+       // n = numbers (0-9)
+       cout<<endl;
+       return 0;
+}
