0e124335923c5f47cc11b886fcf0f299d12e54b2
Ricardo Macias <r.macias@samsung.com>
2020-09-22 09:01:03 -0700

    noticed i need to add array of c and d

--- numbCombination/integrationInput.cpp
+++ numbCombination/integrationInput.cpp
@@ -2,24 +2,120 @@
 #include <iostream>
 using namespace std;
 
+int checkDigitEqualsInDiffPosition(int a, int digit,  int currpos, int pos, bool *&visiteda,bool *&visitedb, int &total_d)
+{
+
+   if (a == 0 ) {
+       return total_d;
+    } else {
+        //cout <<"current "<< currentNumber%10 << " currpos "<< currpos <<" isCurrposVisited "<<  visitedPos[currpos]<<endl;
+        //cout <<" digit "<< digit << " pos "<< pos <<" isPosVisited "<<  visitedPosV2[pos]<<endl;
+        if ( a%10 == digit && !visiteda[pos] && !visitedb[currpos]) {
+         
+        // cout << " equals " << digit << " pos "<< pos << " currpos "<< currpos<< endl;
+         visiteda[pos] = true;
+         visitedb[currpos] = true;
+         total_d++;
+       } 
+          currpos-=1;
+          return  checkDigitEqualsInDiffPosition(a/10, digit,  currpos, pos, visiteda, visitedb, total_d);
+    }
+
+}
+
+int checkDigitEqualsInSamePosition(int a, int b, int &total_c, int &pos, bool *&visiteda, bool *&visitedb) {
+    if (a == 0 ) {
+       return total_c;
+    } else {
+       if ( a%10 == b%10 ) {
+          visiteda[pos] = true;
+          visitedb[pos] = true;
+          total_c++;
+       } 
+           pos-=1;
+          return checkDigitEqualsInSamePosition(a/10, b/10,total_c, pos, visiteda, visitedb);
+    }
+}
+
+void operation(int b, int c, int d)
+{
+    bool *visiteda = new bool[5];
+    bool *visitedb = new bool[5];
+    for(int i=0; i<5; ++i)
+    {
+        visiteda[i] = false;
+        visitedb[i] = false;
+    }
+    for (int a =10000; a< 100000; ++a)
+    {
+        int total_c=0, total_d =0,pos=4; 
+        total_c = checkDigitEqualsInSamePosition(a, b, total_c,pos,visiteda,visitedb);
+        int currpos =4, digit=b;
+        //cout << " tota diff pos => "<<merginFunctionsV2( a, digit, b, pos, visitedPos, visitedPosV2, currpos)<<endl; 
+        int rem, posVariable=4;
+        for (int i = 0; i < 5; i++) {
+            rem = digit %10;
+            digit = digit / 10;
+           // cout << rem << " => " <<posVariable << endl;
+            total_d = checkDigitEqualsInDiffPosition(a, rem, currpos, posVariable, visiteda,visitedb,total_d);
+            posVariable--;
+        }
+        if (total_c == c && total_d == d)
+        {   
+            cout << b <<endl;
+            cout << a <<endl;
+            cout << endl<<" total same pos => "<<total_c<< " diff pos -> "<<total_d<< endl;
+            for(int i=0; i<5; ++i)
+            {
+                cout << " "<< visiteda[i] ;
+            
+            }
+            cout<<endl;
+            for(int i=0; i<5; ++i)
+            {
+                cout << " "<< visitedb[i] ;
+            
+            }
+            cout <<endl;
+        }
+        //cout << "totnum diff pos => "<< total_d <<endl;
+        for(int i=0; i<5; ++i)
+        {
+            visiteda[i] = false;
+            visitedb[i] = false;
+        }
+    }
+    /*
+    for (int i=0; i<5; i++)
+    {
+      cout <<" " << visiteda[i];
+    }
+    cout << endl;
+    for (int i=0; i<5; i++)
+    {
+      cout <<" " << visitedb[i];
+    }
+    cout << endl;
+    */
+}
 int main()
 {
     freopen("input.txt","r", stdin);
 
     int t;
     cin >> t;
-    for(int tc=1; tc<= t; ++tc)
+    for(int tc=1; tc<= 1; ++tc)
     {
         int st;
         cin >> st;
         cout << " "<< st<<endl;
         int b,c,d;
-        for(int i=1; i <=st; ++i)
+        for(int i=1; i <=1; ++i)
         {
 
             cin >> b;cin >> c; cin>>d;
             cout << b << " " << c << " "<< d <<endl;
-
+            operation(b, c,  d);
         }
     
 
