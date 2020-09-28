b63e016b13f7511c28049a2be21f0ba4f1c41b19
Ricardo Macias <r.macias@samsung.com>
2020-09-28 09:02:54 -0700

    building the code

--- samsungbonus/firstTest.cpp
+++ samsungbonus/firstTest.cpp
@@ -1,6 +1,98 @@
 #include <iostream>
 using namespace std;
 
+
+  struct node
+  {
+    int data;
+    node *next;
+  };	
+
+
+    class list
+  {
+    private:
+    node *head, *tail;
+    public:
+    list()
+    {
+      head=NULL;
+      tail=NULL;
+    }
+
+  void createnode(int value)
+    {
+      node *temp=new node;
+      temp->data=value;
+      temp->next=NULL;
+      if(head==NULL)
+      {
+        head=temp;
+        tail=temp;
+        temp=NULL;
+      }
+      else
+      {	
+        tail->next=temp;
+        tail=temp;
+      }
+    }
+
+
+  void display()
+  {
+    node *temp=new node;
+    temp=head;
+    while(temp!=NULL)
+    {
+      cout<<temp->data<<"\t";
+      temp=temp->next;
+    }
+  } 
+    bool ExpreVal()
+    {
+        node *temp=new node;
+        temp=head; 
+        if (temp == NULL || temp->data < 0 )
+        {
+            return false;
+        }
+        if (tail == NULL || tail->data < 0)
+        {
+            return false;
+        }
+            
+        if (temp != NULL && temp->data >0)
+        {
+
+            int current;
+            while(temp!=NULL)
+            {
+                current = temp->data;
+                //temp->data;
+                temp=temp->next;
+                if(current < 0 && temp->data < 0)
+                    return false;    
+                        
+            }
+        }
+        return true;        
+    }
+
+  };	
+
+list  populateList(list lt, int arr[], int size)
+{
+
+    int i,j;
+    for(i=0; i<size; i++)
+    {
+      //  cout<< " "<< arr[i];
+        lt.createnode(arr[i]);
+    }
+    return lt;
+}
+
 //function to print the array
 void printarray(int arr[], int size)
 {
@@ -24,7 +116,12 @@ void permutation(int *arr, int start, int end)
 {
     if(start==end)
     {
+        list ls;
+        ls =  populateList(ls,arr, end+1);
         printarray(arr, end+1);
+        ls.display();
+        cout<<endl;
+        cout << ls.ExpreVal()<<endl;
         return;
     }
     int i;
